a1aff4bb555a5bd8b854dc2499ac5e73746981e8
Shashi Bhushan <sbhushan1@outlook.com>
2017-06-29 15:06:23 +0530

    Week -2 Submit Assignment

--- Week 2/machine-learning-ex1/ex1/gradientDescentMulti.m
+++ Week 2/machine-learning-ex1/ex1/gradientDescentMulti.m
@@ -24,10 +24,7 @@ for iter = 1:num_iters
     % h is a number
 	% what i need to do with this number is theta(zero) := theta(zero) - X[row][zero]
 	
-	theta(1) = theta(1) - (alpha/m) * h  * X(:, 1);
-	theta(2) = theta(2) - (alpha/m) * h  * X(:, 2);
-	theta(3) = theta(3) - (alpha/m) * h  * X(:, 3);
-
+	theta = (theta - (alpha/m) * h  * X)(1,:)';
     % ============================================================
 
     % Save the cost J in every iteration    
